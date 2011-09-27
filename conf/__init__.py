6a3cf659d77703814bdd9602d44d18a71101c525
James McKernan <james@alki.me>
2011-09-27 07:07:57 -0700

    a) add common.cfg b) rename common/backend.py for clarification/code encapsulation

--- conf/__init__.py
+++ conf/__init__.py
@@ -13,16 +13,20 @@ from config import Config
 class ConfigException(Exception):
     pass
 
-ENV_NAME = 'REALCHAT_CONFIG_FILE'
+ENV_NAME = 'REALCHAT_ENV'
 if ENV_NAME not in os.environ:
     raise ConfigException(
         'Unknown environment (%s environ variable not set)' % ENV_NAME)
-CONF_FILENAME = os.environ[ENV_NAME]
+BASE_PATH        = os.path.split(os.path.abspath(__file__))[0]
+CONF_FILE_COMMON = os.path.join(BASE_PATH, '%s.cfg' % 'common')
+CONF_FILE_ENV    = os.path.join(BASE_PATH, '%s.cfg' % os.environ[ENV_NAME])
 THE_CONF = None
 def load(_reload=False):
     global THE_CONF
     if _reload or THE_CONF is None:
-        THE_CONF = Config(CONF_FILENAME) 
+        THE_CONF = Config()
+        THE_CONF.load(file(CONF_FILE_COMMON))
+        THE_CONF.load(file(CONF_FILE_ENV))
 load()
 
 def get(name, default = None):
