aa0d6c5adc4f9cc56d002115989478b9e1c810ab
James McKernan <james.mckernan@gmail.com>
2012-03-05 17:21:23 -0800

    Added a common file for encrypting and decrypting text. Utilizes the keyczar cryptographic toolkit, which allows for keyset management and does better, safer crypto functions by default.

--- conf/__init__.py
+++ conf/__init__.py
@@ -8,7 +8,7 @@ required for config imports to work.
 '''
 
 import os
-from config import Config
+from config import Config, ConfigMerger
 
 class ConfigException(Exception):
     pass
@@ -20,13 +20,25 @@ if ENV_NAME not in os.environ:
 BASE_PATH        = os.path.split(os.path.abspath(__file__))[0]
 CONF_FILE_COMMON = os.path.join(BASE_PATH, '%s.cfg' % 'common')
 CONF_FILE_ENV    = os.path.join(BASE_PATH, '%s.cfg' % os.environ[ENV_NAME])
+CONF_FILE_LOCAL  = os.path.join(BASE_PATH, '%s.cfg' % 'local')
+if not os.path.exists(CONF_FILE_LOCAL):
+    CONF_FILE_LOCAL = None
 THE_CONF = None
+def _local_resolver(map1, map2, key):
+    return 'overwrite'
+
 def load(_reload=False):
     global THE_CONF
     if _reload or THE_CONF is None:
         THE_CONF = Config()
         THE_CONF.load(file(CONF_FILE_COMMON))
         THE_CONF.load(file(CONF_FILE_ENV))
+        if CONF_FILE_LOCAL is not None:
+            local_conf = Config()
+            merger = ConfigMerger(_local_resolver)
+            local_conf.load(file(CONF_FILE_LOCAL))
+            merger.merge(THE_CONF, local_conf)
+
 load()
 
 def get(name, default = None):
